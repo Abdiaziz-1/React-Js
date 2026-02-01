95faca34459f3ba46eadb603936864e134f1bf1b
Abdiaziz-1 <abdimoham100@gmail.com>
2026-02-01 17:35:39 +0300

    added exercise 3 of react js

---
+++ src/Exercise3/UserCardProps.jsx
@@ -0,0 +1,11 @@
+
+const UserCardProp = ({UserName, UserEmail}) => {
+
+    return(
+           <div>
+                <h1>{UserName}</h1>
+                <p>{UserEmail}</p>
+        </div>
+    )
+}
+export default UserCardProp;
