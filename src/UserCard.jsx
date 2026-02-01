fe1f4d8e4d783ad36578107f87f38fbd0cc56639
Abdiaziz-1 <abdimoham100@gmail.com>
2026-02-01 10:26:40 +0300

    added exercise 1 of react js

---
+++ src/UserCard.jsx
@@ -0,0 +1,11 @@
+
+const UserCard = () => {
+    return(
+        <div>
+            <h1>Abdiaziz Mohamed</h1>
+            <p>abdimoham100@gmail.com</p>
+        </div>
+    )
+}
+
+export default UserCard;
