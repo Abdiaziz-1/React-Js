c959de93a22af22512c86caf9f04ecfee3e8c786
Abdiaziz-1 <abdimoham100@gmail.com>
2026-02-01 17:36:48 +0300

    modified the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import Blog from "./Components/Blog";
+import UserCardProp from "./Exercise3/UserCardProps";
 import UserCard from "./UserCard";
 
 function App() {
+
+
   return(
     <div>
     {/* <UserCard /> */}
-    <Blog />
+    {/* <Blog /> */}
+    <UserCardProp 
+      UserName = "Abdiaziz"
+      UserEmail = "abdimoham100@gmail.com"
+     />
+
+       <UserCardProp 
+      UserName = "Amina"
+      UserEmail = "amina@gmail.com"
+     />
+
+       <UserCardProp 
+      UserName = "Ali"
+      UserEmail = "ali@gmail.com"
+     />
+     
     
     </div>
   )
